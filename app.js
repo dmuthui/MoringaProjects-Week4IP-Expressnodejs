4f3e62aa537dcb159a5ee2ee495aee0f2325bf90
dmuthui <vyda2002us@gmail.com>
2023-01-17 10:10:49 +0300

    Adds changes on the folder

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const express = require('express');
 // Initialize express
 const app = express();
 
+// Set up a view engine
+app.set('view engine', 'ejs');
+
+
 // Set a static folder
 app.use(express.static('public'));
+
 
 // Define the port number
 const PORT = 5000;
 
 app.listen(PORT, ()=>{
-   console.log(`Server is listening on port ${PORT}`)
-})
+    console.log(`Server is listening on port ${PORT}`)
+});
