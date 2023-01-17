d6d2faf860a2425dc87dcf81df3c13f8b5c2e858
dmuthui <vyda2002us@gmail.com>
2023-01-17 10:19:54 +0300

    adds app.js modification

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.set('view engine', 'ejs');
 // Set a static folder
 app.use(express.static('public'));
 
+app.get('/', (req,res)=>{
+    res.render('index');
+ })
 
 // Define the port number
 const PORT = 5000;
